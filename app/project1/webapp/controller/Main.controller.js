b90fd2936255e993386c5239cf5f6cfa0c44a4a1
Emre Aydin <kodaskina@gmail.com>
2025-08-26 20:33:42 +0000

    Search filter added

--- app/project1/webapp/controller/Main.controller.js
+++ app/project1/webapp/controller/Main.controller.js
@@ -1,26 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel"
+], (Controller,JSONModel) => {
     "use strict";
 
     return Controller.extend("project1.controller.Main", {
         onInit() {
         },
-        onGetNews: function () {
+        onSearchNews: function () {
+            const sQuery = this.byId("idSearchQuery").getValue();
+            const sLang = this.byId("idSelectLang").getSelectedKey() || "tr";
+            const iPageSize = parseInt(this.byId("idPageSize").getValue(), 10) || 5;
+
+            const sParameter = {
+                q: sQuery || "tesla",
+                fromDate: "2025-07-26",
+                sortBy: "publishedAt",
+                language: sLang,
+                page: 1,
+                pageSize: iPageSize
+            };
+
             $.ajax({
                 url: "/catalog/news/Articles",
-                data: {
-                    q: "tesla",
-                    fromDate: "2025-07-26",
-                    sortBy: "publishedAt",
-                    language: "tr",
-                    page: 1,
-                    pageSize: 50
-                },
+                data: sParameter,
                 success: function (oData) {
-                    console.log("News data:", oData);
-
-                    const oJson = new sap.ui.model.json.JSONModel(oData);
+                    const oJson = new JSONModel(oData);
                     this.getView().setModel(oJson, "news");
                 }.bind(this),
                 error: function (oError) {
